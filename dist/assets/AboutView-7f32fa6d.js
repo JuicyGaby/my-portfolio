1687ecf2ae1b1c653843aa52028810ef5e3b4454
Gabe Fletcher Sanchez <78129712+JuicyGaby@users.noreply.github.com>
2023-05-16 17:06:19 +0800

    adding dist

---
+++ dist/assets/AboutView-7f32fa6d.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t}from"./index-6d708483.js";const c={};function r(s,n,a,_,p,f){return o(),t("div")}const d=e(c,[["render",r]]);export{d as default};
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template lang="">
   <h1>{{ name }}</h1>
+  <h2>This is my first build and github pages</h2>
   <v-btn variant="flat" color="success">text</v-btn>
 </template>
 <script>
