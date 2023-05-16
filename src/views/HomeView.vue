208493a6181f2cae7a6b266101edf90415376b6b
Gabe Fletcher Sanchez <78129712+JuicyGaby@users.noreply.github.com>
2023-05-16 21:37:42 +0800

    Doc : Declared the divs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,57 @@
-<template>
-  <HelloWorld />
+<template lang="">
+    <div class="main">
+        <div class="navbar">
+            
+        </div>
+    </div>
+    <div class="description">
+
+    </div>
+    <div class="skills">
+
+    </div>
+    <div class="works">
+
+    </div>
+    <div class="footer">
+
+    </div>
 </template>
-
 <script>
-import { defineComponent } from 'vue';
+export default {
 
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'HomeView',
-
-  components: {
-    HelloWorld,
-  },
-});
+}
 </script>
+<style lang="css" scoped>
+    /* main starts */
+    .main {
+        height: 100vh;
+        width: 100%;
+        background-color: lightblue;
+    }
+    .navbar {
+        height: 10%;
+    }
+
+    /* main ends */
+    .description {
+        height: 30vh;
+        width: 100%;
+        background-color: lightgreen;
+    }
+    .skills {
+        height: 70vh;
+        width: 100%;
+        background-color: lightblue;
+    }
+    .works {
+        height: 100vh;
+        width: 100%;
+        background-color: lightgreen;
+    }
+    .footer {
+        height: 20vh;
+        width: 100%;
+        background-color: lightblue;
+    }
+</style>
