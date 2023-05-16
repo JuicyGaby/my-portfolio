bb6541eb2f6bba4ce580d006794b44d5cc9500b2
Gabe Fletcher Sanchez <78129712+JuicyGaby@users.noreply.github.com>
2023-05-16 22:53:26 +0800

    Feat : Added smooth scroll

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,75 @@
 <template lang="">
-    <div class="main">
-        <div class="navbar">
-            
+    <div class="main" id="about">
+        <div class="navbar d-flex justify-space-between align-center py-2 px-3">
+            <div class="">JuicyGaby</div>
+            <div class="anchor">
+                <ul class="d-inline-flex no-text-decoration">
+                    <li><a @click="scrollToElement('#about')">About Me</a></li>
+                    <li><a @click="scrollToElement('#skills')">Skills</a></li>
+                    <li><a @click="scrollToElement('#projects')">Projects</a></li>
+                    <li><a @click="scrollToElement('#contact')">Contact</a></li>
+                </ul>
+            </div>
         </div>
     </div>
-    <div class="description">
+    <div class="description" id="">
 
     </div>
-    <div class="skills">
+    <div class="skills" id="skills">
 
     </div>
-    <div class="works">
+    <div class="projects" id="projects">
 
     </div>
-    <div class="footer">
+    <div class="footer" id="contact">
 
     </div>
 </template>
 <script>
-export default {
 
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        scrollToElement(elementId) {
+            const destination = document.querySelector(elementId)
+            window.scrollTo({
+                behavior: 'smooth',
+                top: destination.offsetTop
+            })
+        }
+    },
 }
 </script>
 <style lang="css" scoped>
     /* main starts */
+    html {
+        scroll-behavior: smooth;
+    }
     .main {
-        height: 100vh;
+        height: 70vh;
         width: 100%;
         background-color: lightblue;
+        border: 1px solid black;
+    }
+    div {
+        border: 1px solid black;
+    }
+    ul li {
+        list-style: none;
+        margin-left: 10em;
+        
+    }
+    a {
+        text-decoration: none;
+        cursor: pointer;
     }
     .navbar {
         height: 10%;
+        border: 1px solid black;
     }
 
     /* main ends */
@@ -40,11 +79,11 @@ export default {
         background-color: lightgreen;
     }
     .skills {
-        height: 70vh;
+        height: 100vh;
         width: 100%;
         background-color: lightblue;
     }
-    .works {
+    .projects {
         height: 100vh;
         width: 100%;
         background-color: lightgreen;
