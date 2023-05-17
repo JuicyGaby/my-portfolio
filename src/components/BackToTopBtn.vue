5294d7fb84815f43687e6e218255b0144fc7ba04
Gabe Fletcher Sanchez <78129712+JuicyGaby@users.noreply.github.com>
2023-05-17 17:15:56 +0800

    Feat : added back to top btn

---
+++ src/components/BackToTopBtn.vue
@@ -0,0 +1,48 @@
+<template lang="">
+    <transition name="fade">
+        <v-btn
+        v-if="showButton"
+        icon="mdi-chevron-up"
+        class="back-to-top-button"
+        @click="scrollToTop"
+        >
+        </v-btn>
+    </transition>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            showButton: false,
+        }
+    },
+    mounted() {
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    beforeDestroy() {
+        window.removeEventListener('scroll', this.handleScroll);
+    },
+    methods: {
+        handleScroll() {
+            this.showButton = window.pageYOffset > 200
+        },
+        scrollToTop() {
+            console.log('sdfds');
+            window.scrollTo({top : 0, behavior : 'smooth'})
+        }
+    },
+}
+</script>
+
+<style>
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 3s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,55 @@
 <template lang="">
-    <div class="main" id="about">
-        <div class="navbar d-flex justify-space-between align-center py-2 px-3">
-            <div class="">JuicyGaby</div>
-            <div class="anchor">
-                <ul class="d-inline-flex no-text-decoration">
-                    <li><a @click="scrollToElement('#about')">About Me</a></li>
-                    <li><a @click="scrollToElement('#skills')">Skills</a></li>
-                    <li><a @click="scrollToElement('#projects')">Projects</a></li>
-                    <li><a @click="scrollToElement('#contact')">Contact</a></li>
-                </ul>
-            </div>
-        </div>
+    <div class="topBtn">
+        <BackToTopBtn />
     </div>
-    <div class="description" id="">
 
+    <div class="main" id="about">
+        <v-toolbar color="primary" class="px-10">
+            <v-toolbar-title>
+                JuicyGaby
+            </v-toolbar-title>
+            <ul class="d-inline-flex no-text-decoration">
+                <li><a @click="scrollToElement('#about')">About Me</a></li>
+                <li><a @click="scrollToElement('#skills')">Skills</a></li>
+                <li><a @click="scrollToElement('#projects')">Projects</a></li>
+                <li><a @click="scrollToElement('#contact')">Contact</a></li>
+            </ul>
+        </v-toolbar>
+        
+    </div>
+    <div class="description d-flex flex-column justify-center align-center" id="">
+        <h2 class="mb-3">About Me</h2>
+        <div class="d-flex justify-space-between align-center">
+    
+            <v-icon icon="mdi-information" class="mr-5" size="x-large"></v-icon>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Vitae deserunt <br> laudantium odit quo temporibus soluta qui
+                  commodi modi veritatis ducimus.</p>
+        </div>
+        
+        
     </div>
     <div class="skills" id="skills">
 
     </div>
     <div class="projects" id="projects">
-
     </div>
+    <!-- <BackToTopBtn /> -->
     <div class="footer" id="contact">
-
     </div>
 </template>
 <script>
 
+// importing vue components
+import BackToTopBtn from '../components/BackToTopBtn.vue'
+
 export default {
+    components : {
+      BackToTopBtn,
+    },
     data() {
         return {
-            
+
         }
     },
     methods: {
@@ -45,52 +64,61 @@ export default {
 }
 </script>
 <style lang="css" scoped>
-    /* main starts */
-    html {
-        scroll-behavior: smooth;
-    }
-    .main {
-        height: 70vh;
-        width: 100%;
-        background-color: lightblue;
-        border: 1px solid black;
-    }
-    div {
-        border: 1px solid black;
-    }
-    ul li {
-        list-style: none;
-        margin-left: 10em;
-        
-    }
-    a {
-        text-decoration: none;
-        cursor: pointer;
-    }
-    .navbar {
-        height: 10%;
-        border: 1px solid black;
-    }
+/* main starts */
+html {
+    scroll-behavior: smooth;
+}
 
-    /* main ends */
-    .description {
-        height: 30vh;
-        width: 100%;
-        background-color: lightgreen;
-    }
-    .skills {
-        height: 100vh;
-        width: 100%;
-        background-color: lightblue;
-    }
-    .projects {
-        height: 100vh;
-        width: 100%;
-        background-color: lightgreen;
-    }
-    .footer {
-        height: 20vh;
-        width: 100%;
-        background-color: lightblue;
-    }
+.topBtn {
+    position: fixed;
+    bottom: 8%;
+    right: 5%;
+}
+.main {
+    height: 80vh;
+    width: 100%;
+    background-color: lightblue;
+    border: 1px solid black;
+}
+
+
+ul li {
+    list-style: none;
+    margin-left: 5rem
+}
+
+a {
+    text-decoration: none;
+    cursor: pointer;
+}
+
+.navbar {
+    height: 10%;
+    border: 1px solid black;
+}
+
+/* main ends */
+.description {
+    height: 20vh;
+    width: 100%;
+    background-color: lightgreen;
+}
+
+.skills {
+    height: 100vh;
+    width: 100%;
+    background-color: lightblue;
+}
+
+.projects {
+    height: 100vh;
+    width: 100%;
+    background-color: lightgreen;
+}
+
+.footer {
+    height: 20vh;
+    width: 100%;
+    background-color: lightblue;
+}
 </style>
