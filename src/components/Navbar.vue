9c9e782aeccb2b048e8f1b9f1fd6ccf38b44afa4
Gabe Fletcher Sanchez <78129712+JuicyGaby@users.noreply.github.com>
2023-07-13 01:22:23 +0800

    Doc changes in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,21 +4,25 @@
       <v-container fluid>
         <v-row>
           <v-col class="d-flex align-center">
-            <p class="text-h6 text-white">JuicyGaby</p>
+            <a class="text-h5" href="https://github.com/JuicyGaby">
+              <div class="d-flex align-center">
+                <v-icon icon="mdi-github"></v-icon>
+                <p class="font-weight-medium mx-2">JuicyGaby</p>
+              </div>
+            </a>
           </v-col> 
           <v-col class="hidden-sm-and-down">
             <div class="d-flex justify-space-between">
-              <a class="text-h6" href="#skills">About Me</a>
-              <a class="text-h6" href="#skills">Skills</a>
-              <a class="text-h6" href="#skills">Projects</a>
-              <a class="text-h6" href="#skills">Contacts</a>
+              <div v-for="link in navLinks">
+                <a class="text-h6" :href="link.link" >{{link.tag}}</a>
+              </div>
             </div>
           </v-col>
           <v-col class="hidden-md-and-up">
             <div class="d-flex justify-end">
-              <v-btn size="x-large" variant="plain">
-                <v-icon :icon="drawerOpen ? 'mdi-menu-open' : 'mdi-menu'" color="#F0E3CA" @click="drawerOpen = !drawerOpen"></v-icon>
-              </v-btn>
+              <p class="text-h5">
+                  <v-icon :icon="drawerOpen ? 'mdi-menu-open' : 'mdi-menu'" color="#F0E3CA" @click="drawerOpen = !drawerOpen"></v-icon>
+              </p>
             </div>
           </v-col>
         </v-row>
@@ -31,6 +35,12 @@ export default {
   data() {
     return {
       drawerOpen: false,
+      navLinks: [
+        { tag: 'About Me', link: '#about'},
+        { tag: 'Skills', link: '#skills'},
+        { tag: 'Projects', link: '#projects'},
+        { tag: 'Contacts', link: '#contacts'},
+      ]
     }
   },
 }
@@ -42,5 +52,9 @@ div {
 a {
   text-decoration: none;
   color: var(--textColor);
+  transition: all .3s;
+}
+a:hover {
+  color: #FF8303;
 }
 </style>
